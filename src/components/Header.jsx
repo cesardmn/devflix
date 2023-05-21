0a9b4d3a8f575912fe14db462d92a4151e0b2888
cesardmn <cesardmn@gmail.com>
2023-05-21 09:45:25 -0300

    create logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 // styles
 import styles from "@styles/Header.module.css";
+import Logo from "./Logo";
 
 export default function Header() {
   return (
     <header className={styles.header}>
-      <div className={styles.logo}>DEVIFLIX</div>
+      <div className={styles.logo}><Logo /></div>
       <div className={styles.searchBar}>search bar</div>
       <div className={styles.avatar}>avatar</div>
     </header>
---
+++ src/components/Logo.jsx
@@ -0,0 +1,5 @@
+import styles from '@styles/Logo.module.css'
+
+export default function Logo() {
+  return <div className={styles.logo}>DEVFLIX</div>
+}
