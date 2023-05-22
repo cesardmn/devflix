2c6ec6a08be2ca81c4e8a68de49b894bab9ae6e4
cesardmn <cesardmn@gmail.com>
2023-05-22 06:34:59 -0300

    SearchBar component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,13 @@
 // styles
 import styles from "@styles/Header.module.css";
 import Logo from "./Logo";
+import SearchBar from "./SearchBar";
 
 export default function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.logo}><Logo /></div>
-      <div className={styles.searchBar}>search bar</div>
+      <div className={styles.searchBar}> <SearchBar /> </div>
       <div className={styles.avatar}>avatar</div>
     </header>
   );
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,14 @@
+// styles
+import styles from "@styles/SearchBar.module.css"
+import { GrSearch } from "react-icons/gr";
+
+export default function SearchBar() {
+  return (
+    <div className={styles.searchBar}>
+      <input type="search" name="" id="" placeholder="Pesquisar" />
+      <div className={styles.iconButton}>
+        <GrSearch />
+      </div>  
+    </div>
+  );
+}
