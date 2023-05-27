f40f9f1d693071cecf6c4b5dde0c599b6eea017b
cesardmn <cesardmn@gmail.com>
2023-05-27 20:50:56 -0300

    add db api

---
+++ src/pages/api/db.jsx
@@ -0,0 +1,74 @@
+export default function handler(req, res) {
+  
+  // todo fetch db.
+
+  const db = [
+    {
+      id: 'lUa-ZDcTnt4',
+      title: 'Como Aprender Java, passo a passo!',
+      description:
+        '5 Dicas Infalíveis de como aprender Java em 2023\n➡…ia.\n\nSó fracassa quem desiste 💪\n\nEmerson Machado',
+      thumb: 'https://i.ytimg.com/vi/lUa-ZDcTnt4/sddefault.jpg',
+      cat: 'Java',
+    },
+    {
+      id: 'aQbCSyvnYs0',
+      title: 'Java ou Kotlin - Qual ESCOLHER em 2023?',
+      description:
+        'Java ou Kotlin - Qual vale a pena Estudar em 2023?…ia.\n\nSó fracassa quem desiste 💪\n\nEmerson Machado',
+      thumb: 'https://i.ytimg.com/vi/aQbCSyvnYs0/sddefault.jpg',
+      cat: 'Java',
+    },
+    {
+      id: 'Ptbk2af68e8',
+      title: 'O que o JavaScript é capaz de fazer? - Curso JavaScript #01',
+      description:
+        'Você sabe para que serve a linguagem JavaScript / …oemVideo #JavaScript #EcmaScript #MóduloC #Aula01',
+      thumb: 'https://i.ytimg.com/vi/Ptbk2af68e8/sddefault.jpg',
+      cat: 'JavaScript',
+    },
+    {
+      id: 'QnLSBuykGxI',
+      title: 'O que é Javascript? Por que devemos saber hoje? O que ele faz?',
+      description:
+        'Quer se aprofundar mais? Aproveite o desconto na A…funciona?\n07:03 - Javascript é Java?\n07:21 - APIs',
+      thumb: 'https://i.ytimg.com/vi/QnLSBuykGxI/sddefault.jpg',
+      cat: 'JavaScript',
+    },
+    {
+      id: 'Dp-biEtYiO4',
+      title: 'O que Estudar p/ Trabalhar com Python [GUIA Completo]',
+      description:
+        'Quer Viver de Python e Automação? Matricule-se➡ ht…------------------------\njhonatan@devaprender.com',
+      thumb: 'https://i.ytimg.com/vi/Dp-biEtYiO4/sddefault.jpg',
+      cat: 'Python',
+    },
+    {
+      id: 'Mp0vhMDI7fA',
+      title: 'Curso Python #02 - Para que serve o Python?',
+      description:
+        'De onde vem o Python? Por que esse nome? Quais são…le+: http://plus.google.com/112666558837414979080',
+      thumb: 'https://i.ytimg.com/vi/Mp0vhMDI7fA/sddefault.jpg',
+      cat: 'Python',
+    },
+    {
+      id: 'H43fXodv6WY',
+      title: 'PHP vale a pena? (minha opinião sincera)',
+      description:
+        '✅ 𝗔𝗚𝗢𝗥𝗔 𝗘𝗨 𝗧𝗘𝗡𝗛𝗢 𝗨𝗠 𝗖𝗨𝗥𝗦𝗢 😍\n▸ …conseguir um Emprego\nhttps://youtu.be/fYR9L2ZmodM',
+      thumb: 'https://i.ytimg.com/vi/H43fXodv6WY/sddefault.jpg',
+      cat: 'PHP',
+    },
+    {
+      id: 'AqDj3OSV0mM',
+      title:
+        'PHP (A linguagem mais querida da internet?) // Dicionário do Programador',
+      description:
+        '🤝 𝗛𝗢𝗦𝗧𝗚𝗔𝗧𝗢𝗥 → https://codft.me/HGAqDj3OS…Alves\n\n#PHP #Programacao #DicionarioDoProgramador',
+      thumb: 'https://i.ytimg.com/vi/AqDj3OSV0mM/sddefault.jpg',
+      cat: 'PHP',
+    },
+  ]
+
+  res.status(200).json({ db })
+}
