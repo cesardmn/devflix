8b71bfc2c28600c352472dc13f2184aeba9704f2
cesardmn <cesardmn@gmail.com>
2023-06-02 16:00:47 -0300

    add arrow back in Player

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,23 +4,29 @@ import { useEffect, useState } from 'react'
 // styles
 import styles from '@styles/Home.module.css'
 import { Avatar } from '@mui/material'
+import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace'
 
 //components
 import Logo from '@components/Logo'
 import SearchAppBar from '@src/components/SearchBar'
 import Categories from '@src/components/Categories'
 import CardVideoList from '@src/components/CardVideoList'
+import Player from '@src/components/Player'
 
 // providers
 import { useDb } from '@providers/DbProvider'
 import { useVideos } from '@providers/VideosProvider'
 import { usePlayer } from '@providers/PlayerProvider'
-import Player from '@src/components/Player'
 
 export default function Home() {
   const { setDb } = useDb()
   const { setVideos } = useVideos()
-  const { player } = usePlayer()
+  const { player, setPlayer } = usePlayer()
+
+  const handleHome = () => {
+    console.log('home')
+    setPlayer(false)
+  }
 
   useEffect(() => {
     const fetchData = async () => {
@@ -41,7 +47,14 @@ export default function Home() {
     <div className={styles.app}>
       <header>
         <nav>
-          <Logo />
+          <div
+            className={styles.logo}
+            onClick={() => {
+              handleHome()
+            }}
+          >
+            {player ? <KeyboardBackspaceIcon /> : <Logo />}
+          </div>
           {!player && <SearchAppBar />}
           <Avatar />
         </nav>
