f9324cd050919f8d96ed4023bc95f945e53f9239
cesardmn <cesardmn@gmail.com>
2023-06-02 14:45:32 -0300

    add VideoCardist

--- src/components/CardVideoList.jsx
+++ src/components/CardVideoList.jsx
@@ -7,11 +7,11 @@ import { useVideos } from '@providers/VideosProvider'
 export default function CardVideoList() {
   const { videos } = useVideos()
   return (
-    <div className={styles.cardVideoList}>
+    <ul className={styles.cardVideoList}>
       {videos &&
         videos.map((video) => {
           return <VideoCard video={video} key={video.id} />
         })}
-    </div>
+    </ul>
   )
 }
